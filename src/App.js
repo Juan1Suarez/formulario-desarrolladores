e926d3fb5e051d69540164ccff6318c0050de902
JuanSuarez1 <juansuarez60249@gmail.com>
2024-04-28 22:49:03 -0300

    form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
+import { Typeahead } from 'react-bootstrap-typeahead';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import provincias from './provincias';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 
 function App() {
+  const [provincia, setProvincia] = useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><h1>Bienvenidos al registro de desarrolladores </h1>
+            <Formik
+        initialValues={{
+          fullName: '',
+          dni: '',
+          localidad: '',
+          provincia: ''
+        }}
+        validate={values => {
+          const errors = {};
+
+          if (!values.fullName) {
+            errors.fullName = 'El nombre completo es requerido';
+          }
+
+          if (!values.dni) {
+            errors.dni = 'El DNI es requerido';
+          }
+          return errors;
+        }}
+        onSubmit={(values, actions) => {
+          // Aquí manejas la lógica de envío del formulario
+          console.log(values);
+          actions.setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field
+              type="text"
+              name="Nombre completo"
+              placeholder="Nombre completo"
+            />
+            <ErrorMessage name="fullname" component="div" />
+
+            <Field
+              type="number"
+              name="DNI"
+              placeholder="DNI"
+            />
+            <ErrorMessage name="DNI" component="div" />
+
+            <Typeahead
+              id="typeaheadProvincia"
+              labelKey="nombre"
+              options={provincias}
+              placeholder="Provincia"
+              selected={provincia}
+              onChange={setProvincia}
+            />
+            
+            <Field
+              type="text"
+              name="Localidad"
+              placeholder="Localidad"
+            />
+            <ErrorMessage name="DNI" component="div" />
+
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+
+    </>
   );
 }
 
---
+++ src/provincias.js
@@ -0,0 +1,71 @@
+export default [
+      {
+        "nombre": "Buenos Aires"
+      },
+      {
+        "nombre": "Catamarca"
+      },
+      {
+        "nombre": "Chaco"
+      },
+      {
+        "nombre": "Chubut"
+      },
+      {
+        "nombre": "Córdoba"
+      },
+      {
+        "nombre": "Corrientes"
+      },
+      {
+        "nombre": "Entre Ríos"
+      },
+      {
+        "nombre": "Formosa"
+      },
+      {
+        "nombre": "Jujuy"
+      },
+      {
+        "nombre": "La Pampa"
+      },
+      {
+        "nombre": "La Rioja"
+      },
+      {
+        "nombre": "Mendoza"
+      },
+      {
+        "nombre": "Misiones"
+      },
+      {
+        "nombre": "Neuquén"
+      },
+      {
+        "nombre": "Río Negro"
+      },
+      {
+        "nombre": "Salta"
+      },
+      {
+        "nombre": "San Juan"
+      },
+      {
+        "nombre": "San Luis"
+      },
+      {
+        "nombre": "Santa Cruz"
+      },
+      {
+        "nombre": "Santa Fe"
+      },
+      {
+        "nombre": "Santiago del Estero"
+      },
+      {
+        "nombre": "Tierra del Fuego"
+      },
+      {
+        "nombre": "Tucumán"
+      }
+    ]
